00bd4e90ffb8e853a45c6950e5190e9f449fc482
Fjalar <fjalarhognason@gmail.com>
2025-11-25 16:18:10 +0100

    Day 12 part 2 (first problem with close to a second in runtime, lots of room for improvement by removing copies, not using strings etc.)

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -37,7 +37,39 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut caves = Vec::<Cave>::new();
+
+    input
+        .lines()
+        .map(|line| line.trim().split_once('-').unwrap())
+        .for_each(|(left, right)| {
+            if !caves.iter().any(|cave| cave.name.eq(left)) {
+                caves.push(Cave {
+                    name: left.to_owned(),
+                    paths: Vec::new(),
+                });
+            }
+
+            if !caves.iter().any(|cave| cave.name.eq(right)) {
+                caves.push(Cave {
+                    name: right.to_owned(),
+                    paths: Vec::new(),
+                });
+            }
+
+            let left_cave = caves.iter_mut().find(|cave| cave.name.eq(left)).unwrap();
+            left_cave.paths.push(right.to_owned());
+            let right_cave = caves.iter_mut().find(|cave| cave.name.eq(right)).unwrap();
+            right_cave.paths.push(left.to_owned());
+        });
+
+    // println!("{caves:?}");
+
+    let start = caves.iter().find(|cave| cave.name.eq("start")).unwrap();
+
+    let visited = Vec::new();
+
+    Some(start.count_paths_til_end_part_2(&caves, visited, false))
 }
 
 #[derive(Debug)]
@@ -69,6 +101,41 @@ impl Cave {
                 .sum::<u64>()
         }
     }
+
+    fn count_paths_til_end_part_2(
+        &self,
+        caves: &Vec<Cave>,
+        already_visited_smalls: Vec<String>,
+        has_used_extra_time: bool,
+    ) -> u64 {
+        let mut new_visit_list = already_visited_smalls.clone();
+        new_visit_list.push(self.name.clone());
+        if self.name.eq("end") {
+            // println!("{new_visit_list:?}");
+            1
+        } else {
+            self.paths
+                .iter()
+                .filter(|&path| !path.eq("start"))
+                .map(|path| {
+                    let next_cave = caves.iter().find(|cave| cave.name.eq(path)).unwrap();
+                    if !next_cave.name.eq(&next_cave.name.to_lowercase())
+                        || !already_visited_smalls.contains(&next_cave.name)
+                    {
+                        next_cave.count_paths_til_end_part_2(
+                            caves,
+                            new_visit_list.clone(),
+                            has_used_extra_time,
+                        )
+                    } else if !has_used_extra_time {
+                        next_cave.count_paths_til_end_part_2(caves, new_visit_list.clone(), true)
+                    } else {
+                        0
+                    }
+                })
+                .sum::<u64>()
+        }
+    }
 }
 
 #[cfg(test)]
@@ -84,6 +151,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3509));
     }
 }
